d29dd81f17c592dbb465958b75b416b67eb88047
Alex Erdenberger <aerdenberger@Alexs-MacBook-Pro.local>
2025-07-05 21:54:49 -0700

    casting not spells

--- Chapter2_examples.py
+++ Chapter2_examples.py
@@ -9,3 +9,13 @@ print(f"Your Average Score is: {average:.2f}")
 wholesale_dict = {"name" : "Vacuum", "price" : 130.67}
 
 print(f"Wholesale Price: ${wholesale_dict['price']:.2f}")
+
+def cast_number(number_str_):
+    try:
+        casted_number = float(number_str_)
+    except ValueError:
+        print(f"Couldn't cast {repr(number_str_)} to a number")
+    else:
+        print(f"Casting {repr(number_str_)} to: {casted_number}")
+
+cast_number("123.45")
