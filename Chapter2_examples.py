84e61249cd02dcbdcb4a9a5180e9bad6146518a1
Alex Erdenberger <aerdenberger@Alexs-MacBook-Pro.local>
2025-07-06 20:07:24 -0700

    added nots for constructors, iterables, and eval

--- Chapter2_examples.py
+++ Chapter2_examples.py
@@ -10,6 +10,8 @@ wholesale_dict = {"name" : "Vacuum", "price" : 130.67}
 
 print(f"Wholesale Price: ${wholesale_dict['price']:.2f}")
 
+####################################
+
 def cast_number(number_str_):
     try:
         casted_number = float(number_str_)
@@ -19,3 +21,14 @@ def cast_number(number_str_):
         print(f"Casting {repr(number_str_)} to: {casted_number}")
 
 cast_number("123.45")
+
+###################################
+
+list_str = "[1, 2, 3, 4, 5]"
+stripped_list = list_str.strip("[]")
+number_list = {int(num) for num in stripped_list.split(",")}
+
+print(f"Number List: {number_list}")
+#expected output: {1, 2, 3, 4, 5}
+
+####################################
